506c7264214214c88769c8798a1a2fccc156b8c1
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-08-20 18:13:15 +0200

    :wrench: Change docs url

--- src/auth_service/api.py
+++ src/auth_service/api.py
@@ -6,5 +6,5 @@ from auth_service.router import auth_router
 
 class AuthService:
     def __init__(self, config: AuthConfig) -> None:
-        self.api = FastAPI(version=config.version, title=config.settings.auth_title)
+        self.api = FastAPI(version=config.version, title=config.settings.auth_title, docs_url="/")
         self.api.include_router(router=auth_router)
