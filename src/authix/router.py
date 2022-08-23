579af03f24afca4f7d62468a1259078fc8426a69
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-08-23 19:41:28 +0200

    :white_check_mark: Use headers for access token and HTTP only cookie for refresh

--- src/authix/router.py
+++ src/authix/router.py
@@ -1,11 +1,14 @@
 from typing import Any
 
 from fastapi import APIRouter
+from fastapi import Cookie
 from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Header
+from fastapi import Response
 
 from authix.core.config import AuthConfig
 from authix.core.service_container import ServiceContainer
-from authix.domain.authentication.service import AuthResponse
 from authix.domain.authentication.service import AuthenticationService
 from authix.domain.client.service import ClientImplementationService
 from authix.domain.key.service import KeyService
@@ -22,31 +25,36 @@ REGISTRATION_SERVICE = Depends(service_manager.registration_service)
 KEY_SERVICE = Depends(service_manager.key_service)
 
 
-@auth_router.post("/login", response_model=AuthResponse)
-async def login(email: str, password: str, service: AuthenticationService = AUTH_SERVICE) -> AuthResponse:
+@auth_router.post("/login", response_model=None)
+async def login(email: str, password: str, response: Response, service: AuthenticationService = AUTH_SERVICE) -> None:
     """Authenticate a user by creating a new refresh token, that can be used to create new JWTs."""
-    return await service.authenticate(email=email, password=password)
+    auth_response = await service.authenticate(email=email, password=password)
+    response.set_cookie("refresh_token", auth_response.refresh_token, httponly=True)
+    response.headers["Authorization"] = f"JWT {auth_response.access_token}"
 
 
 @auth_router.delete("/logout", response_model=None)
-async def logout(refresh_token: str, service: RevocationService = REVOCATION_SERVICE) -> None:
+async def logout(refresh_token: str = Cookie(default=None), service: RevocationService = REVOCATION_SERVICE) -> None:
     """Log a user out by deleting their refresh token, and broadcasting a revoke event."""
     return await service.revoke(refresh_token=refresh_token)
 
 
 @auth_router.post("/access_token", response_model=str)
-async def create_access_token(refresh_token: str, service: AuthenticationService = AUTH_SERVICE) -> str:
+async def create_access_token(refresh_token: str = Cookie(...), service: AuthenticationService = AUTH_SERVICE) -> str:
     """Create a new JWT access token from a refresh token."""
     return await service.create_access_token(refresh_token=refresh_token)
 
 
 @auth_router.get("/test", response_model=dict[str, Any])
 async def test_access_token(
-    access_token: str,
+    authorization: str = Header(...),
     service: ClientImplementationService = CLIENT_IMPLEMENTATION_SERVICE,
 ) -> dict[str, Any]:
     """Client implementation of Auth Service."""
-    return await service.client_implementation(access_token=access_token)
+    if authorization is None:
+        raise HTTPException(400)
+    debug(authorization)
+    return await service.client_implementation(access_token=authorization)
 
 
 @auth_router.post("/register", response_model=None)
--- tests/integration_tests/test_router.py
+++ tests/integration_tests/test_router.py
@@ -13,9 +13,8 @@ def valid_login(client: TestClient) -> tuple[str, str]:
 
     response = client.post("/login", params=query_params)
     assert response.status_code == HTTP_200_OK
-    token_dict = response.json()
-    access_token: str = token_dict["access_token"]
-    refresh: str = token_dict["refresh_token"]
+    access_token: str = response.headers["Authorization"].split(" ")[1]
+    refresh: str = response.cookies["refresh_token"]
     return access_token, refresh
 
 
@@ -26,15 +25,15 @@ def register(query_params: dict[str, str], client: TestClient) -> None:
 
 def test_access_token(client: TestClient) -> None:
     access_token, _ = valid_login(client=client)
-    test_response = client.get("/test", params={"access_token": access_token})
+    test_response = client.get("/test", headers={"Authorization": access_token})
     assert test_response.status_code == HTTP_200_OK
 
 
 def test_revoke(client: TestClient) -> None:
     access_token, refresh = valid_login(client=client)
-    response = client.delete("/logout", params={"refresh_token": refresh})
+    response = client.delete("/logout", cookies={"refresh_token": refresh})
     assert response.status_code == HTTP_200_OK
-    test_response = client.get("/test", params={"access_token": access_token})
+    test_response = client.get("/test", headers={"Authorization": access_token})
     assert test_response.status_code == HTTP_401_UNAUTHORIZED
 
 
